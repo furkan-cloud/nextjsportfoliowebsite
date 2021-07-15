dd940b8738469fe23512f9056a962738ef76e073
furkan-cloud <firstinfirstout2020@gmail.com>
2021-07-15 14:45:30 +0300

    timeline section created

--- src/components/Technologies/Technologies.js
+++ src/components/Technologies/Technologies.js
@@ -4,9 +4,52 @@ import { Section, SectionDivider, SectionText, SectionTitle } from '../../styles
 import { List, ListContainer, ListItem, ListParagraph, ListTitle } from './TechnologiesStyles';
 
 const Technologies = () =>  (
-  <div>
-    Technologies
-  </div>
+<Section id="tech">
+  <SectionDivider />
+  <br/>
+  <SectionTitle>Technologies</SectionTitle>
+<SectionText>
+  I'm working with lots of technologies but mainly with react and react native
+</SectionText>
+<List>
+  <ListItem>
+    <DiReact size="3rem" />
+    <ListContainer>
+      <ListTitle>
+        Front-End
+      </ListTitle>
+      <ListParagraph>
+        Experience with <br/>
+        React.js and Javascript
+      </ListParagraph>
+    </ListContainer>
+      </ListItem>
+      <ListItem>
+    <DiFirebase size="3rem" />
+    <ListContainer>
+      <ListTitle>
+        Back-End
+      </ListTitle>
+      <ListParagraph>
+        Experience with <br/>
+       Nodejs and Django
+      </ListParagraph>
+    </ListContainer>
+      </ListItem>
+      <ListItem>
+    <DiZend size="3rem" />
+    <ListContainer>
+      <ListTitle>
+        Mobile
+      </ListTitle>
+      <ListParagraph>
+        Experience with <br/>
+        React Native
+      </ListParagraph>
+    </ListContainer>
+      </ListItem>
+</List>
+</Section>
 );
 
 export default Technologies;
